b2fe9316b67c4ec011c2fe2f35e5bd9dde2fc49f
iamb4uc <103022474+iamb4uc@users.noreply.github.com>
2023-02-26 18:13:05 +0530

    removed suckless stuff to their own repos

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -8,15 +8,12 @@ killall slstatus
 killall picom
 killall redshift
 killall syncthing
-killall lf
 
 
 # Startup Programs
 #
 pulseaudio -D &
-# xrandr --output eDP --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-A-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DisplayPort-0 --off
-# xrandr --output eDP --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DisplayPort-0 --off
-xrandr --output eDP --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output HDMI-A-0 --rate 75 --mode 1920x1080 --pos 0x0 --rotate normal --output DisplayPort-0 --off
+xrandr --output eDP --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-A-0 --mode 1920x1080 --rate 75 --right-of eDP
 xset r rate 300 50
 setxkbmap -option caps:escape &
 slstatus &
