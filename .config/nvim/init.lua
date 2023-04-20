7037b89498df6895fc85c33c33e18959df7a6d39
iamb4uc <103022474+iamb4uc@users.noreply.github.com>
2023-04-20 19:27:16 +0530

    moved config to lazy.nvim with LazyVim configs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,9 @@
 -- ____________________  _______________ ______  __________
 -- ____  _/__    |__   |/  /__  __ )_  // /_  / / /_  ____/
---  __  / __  /| |_  /|_/ /__  __  |  // /_  / / /_  /     
--- __/ /  _  ___ |  /  / / _  /_/ //__  __/ /_/ / / /___   
--- /___/  /_/  |_/_/  /_/  /_____/   /_/  \____/  \____/   
--- A simple single file neovim config
--- github.com/imab4uc for more software
---
+--  __  / __  /| |_  /|_/ /__  __  |  // /_  / / /_  /
+-- __/ /  _  ___ |  /  / / _  /_/ //__  __/ /_/ / / /___
+-- /___/  /_/  |_/_/  /_/  /_____/   /_/  \____/  \____/
+-- A blazingly fast neovim config
+-- github.com/iamb4uc for more software
 
-require('plugins')
-require('settings')
-require('keys')
+require("config.lazy")
