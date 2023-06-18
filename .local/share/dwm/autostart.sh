4556fd82172c7a6a9335b2cf513eed99448aee13
iamb4uc <103022474+iamb4uc@users.noreply.github.com>
2023-06-18 16:25:15 +0530

    changed keyboard speed to make it faster

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -2,15 +2,13 @@
 #
 killall redshift &
 
-rm $HOME/.config/mpdnotify/pid &
 pulseaudio -D &
 xrandr --output eDP --primary --mode 1920x1080 --output HDMI-A-0 --mode 1920x1080 --rate 75 --same-as eDP &
-xset r rate 300 50 &
+xset r rate 250 60 &
 setxkbmap -option caps:escape &
 slstatus &
 setbg &
 picom &
 syncthing &
-mpdnotify start &
 redshift &
 mpd &
