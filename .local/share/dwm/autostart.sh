1e56e8ea988cc07b373f616987ee9ae473c404ea
iamb4uc <103022474+iamb4uc@users.noreply.github.com>
2024-07-06 00:23:11 +0530

    updated autostart

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 #
-killall redshift &
-
 pulseaudio -D &
-xrandr --output eDP --primary --mode 1920x1080 --output HDMI-A-0 --mode 1920x1080 --rate 75 --same-as eDP &
+xrandr --output eDP --primary --mode 1920x1080 --output HDMI-A-0 --mode 1920x1080 --rate 75 --above eDP &
 xset r rate 250 60 &
 setxkbmap -option caps:escape &
 slstatus &
+# setbg /home/iamb4uc/Pictures/wallpapers &
 setbg &
 picom &
 syncthing &
-redshift &
 mpd &
+# mpv /home/iamb4uc/Git/dots/music/intro1.mp3
