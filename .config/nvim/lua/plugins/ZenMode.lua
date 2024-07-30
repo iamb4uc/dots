97922841fae1807b6a690c1f9f50d753b3ce726f
iamb4uc <103022474+iamb4uc@users.noreply.github.com>
2024-07-30 06:59:26 +0530

    updated nvim config

--- .config/nvim/lua/plugins/ZenMode.lua
+++ .config/nvim/lua/plugins/ZenMode.lua
@@ -17,7 +17,7 @@ return {
 				-- * a percentage of the width / height of the editor when <= 1
 				-- * a function that returns the width or the height
 				width = 0.65, -- width of the Zen window
-				height = 0.75, -- height of the Zen window
+				height = 0.90, -- height of the Zen window
 				-- by default, no options are changed for the Zen window
 				-- uncomment any of the options below, or add other vim.wo options you want to apply
 				options = {
