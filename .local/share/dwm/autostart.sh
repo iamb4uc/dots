8b2ae357e7299ade239925fec1df65d821c912d3
iamb4uc <103022474+iamb4uc@users.noreply.github.com>
2025-04-03 19:13:17 +0530

    update

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 pulseaudio -D &
-xrandr --output HDMI-A-0 --primary --mode 1920x1080 --rate 75 --output eDP --mode 1920x1080 --rate 60 --same-as HDMI-A-0 &
+xrandr --output HDMI-A-0 --primary --mode 1920x1080 --rate 75 --output eDP --mode 1920x1080 --rate 60 --below HDMI-A-0 &
 xset r rate 250 60 &
 setxkbmap -option caps:escape &
 slstatus &
