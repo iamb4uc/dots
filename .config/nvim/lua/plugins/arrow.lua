645fa05450b74565fbe6eeabfe5a207f5abaa065
iamb4uc <103022474+iamb4uc@users.noreply.github.com>
2025-08-15 15:53:51 +0530

    new plugin that is a better alternative to harpoon ig

---
+++ .config/nvim/lua/plugins/arrow.lua
@@ -0,0 +1,13 @@
+return {
+	"otavioschwanck/arrow.nvim",
+	dependencies = {
+		-- { "nvim-tree/nvim-web-devicons" },
+		-- or if using `mini.icons`
+		{ "echasnovski/mini.icons" },
+	},
+	opts = {
+		show_icons = true,
+		leader_key = ";", -- Recommended to be a single key
+		buffer_leader_key = "m", -- Per Buffer Mappings
+	},
+}
