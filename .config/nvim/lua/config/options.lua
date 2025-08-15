5a850bbbdffbcfacfd46a729d29847855460eb9b
iamb4uc <103022474+iamb4uc@users.noreply.github.com>
2025-08-15 15:52:47 +0530

    more sensible tabs for what I do

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,8 +4,8 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-vim.opt.tabstop = 8
-vim.opt.shiftwidth = 8
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.visualbell = true
 vim.g.autoformat = true
