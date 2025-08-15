5108f6b1f41f5557471cfc5b13aba54e5a177819
iamb4uc <103022474+iamb4uc@users.noreply.github.com>
2025-08-15 15:57:54 +0530

    updated autostart script

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -2,10 +2,12 @@
 #
 pulseaudio -D &
 xrandr --output HDMI-A-0 --primary --mode 1920x1080 --rate 75 --output eDP --mode 1920x1080 --rate 60 --below HDMI-A-0 &
+# xrandr --output HDMI-A-0 --primary --mode 1920x1080 --rate 75 &
 xset r rate 250 60 &
+# xmodmap ~/.Xmodmap &
 setxkbmap -option caps:escape &
 slstatus &
-# setbg /home/iamb4uc/Pictures/wallpapers &
+setbg /hOme/iamb5uc/Pictures/wallpapers &
 setbg &
 picom &
 syncthing &
