fe0d1e88f45a0fa132922d6804f988d2d5552eca
iamb4uc <103022474+iamb4uc@users.noreply.github.com>
2025-08-15 15:54:17 +0530

    idk

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,10 +1,8 @@
 return {
-	-- { "folke/todo-comments.nvim", enabled = false },
 	{ "ggandor/leap.nvim", enabled = false },
 	{ "rcarriga/nvim-notify", enabled = false },
 	{ "stevearc/dressing.nvim", enabled = false },
 	{ "folke/noice.nvim", enabled = false },
-	-- { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 	{ "nvim-pack/nvim-spectre", enabled = false },
 	{ "ggandor/flit.nvim", enabled = false },
 	{ "akinsho/bufferline.nvim", enabled = false },
@@ -12,8 +10,4 @@ return {
 	{ "catppuccin/nvim", enabled = false },
 	{ "folke/flash.nvim", enabled = false },
 	{ "MagicDuck/grug-far.nvim", enabled = false },
-	-- { "echasnovski/mini.surround", enabled = false },
-	-- { "JoosepAlviste/nvim-ts-context-commentstring", enabled = false },
-	-- { "echasnovski/mini.bufremove", enabled = false },
-	--
 }
