ce20452c4c6b10f5a053f7474c1bd3041c5a8994
iamb4uc <103022474+iamb4uc@users.noreply.github.com>
2025-08-15 15:53:10 +0530

    better configs

--- .config/nvim/lua/plugins/ZenMode.lua
+++ .config/nvim/lua/plugins/ZenMode.lua
@@ -30,6 +30,22 @@ return {
 					-- list = false, -- disable whitespace characters
 				},
 			},
+			plugins = {
+				-- disable some global vim options (vim.o...)
+				-- comment the lines to not apply the options
+				options = {
+					enabled = true,
+					ruler = false, -- disables the ruler text in the cmd line area
+					showcmd = false, -- disables the command in the last line of the screen
+					-- you may turn on/off statusline in zen mode by setting 'laststatus'
+					-- statusline will be shown only if 'laststatus' == 3
+					laststatus = 0, -- turn off the statusline in zen mode
+				},
+				twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
+				gitsigns = { enabled = false }, -- disables git signs
+				tmux = { enabled = false }, -- disables the tmux statusline
+				todo = { enabled = false }, -- if set to "true", todo-comments.nvim highlights will be disabled
+			},
 			-- callback where you can add custom code when the Zen window opens
 			on_open = function(win) end,
 			-- callback where you can add custom code when the Zen window closes
